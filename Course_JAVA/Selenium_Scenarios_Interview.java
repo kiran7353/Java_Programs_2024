dca1a298dcb6c2de6520286688d4c0f19dfa0ef5
Kiran7353 <kbellad3@gmail.com>
2024-12-17 15:35:52 +0530

    Comming one problem

--- Course_JAVA/Selenium_Scenarios_Interview.java
+++ Course_JAVA/Selenium_Scenarios_Interview.java
@@ -5,7 +5,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Selenium_Practice1 {
+public class Selenium_Scenarios_Interview {
     public static void main(String[] args){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
