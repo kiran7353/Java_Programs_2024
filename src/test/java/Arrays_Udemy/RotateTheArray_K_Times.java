1164c8550a67ca93b997de782eeb6cbe9bd7038c
Kiran7353 <kbellad3@gmail.com>
2025-01-15 12:01:24 +0530

    Optimized RotateTheArray_K_Times.java and added Javadoc comments

--- src/test/java/Arrays_Udemy/RotateTheArray_K_Times.java
+++ src/test/java/Arrays_Udemy/RotateTheArray_K_Times.java
@@ -2,29 +2,40 @@ package Arrays_Udemy;
 
 import java.util.Arrays;
 
-/* Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+/**
+ * Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
  */
-
 public class RotateTheArray_K_Times {
 
-    static void reverseArray(int[] arr, int start, int end){
-        int i=start,j=end;
-        while (i < j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+    /**
+     * Reverses a portion of the array from start to end indices.
+     *
+     * @param arr   the array to be reversed
+     * @param start the starting index
+     * @param end   the ending index
+     */
+    static void reverseArray(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 
     public static void main(String[] args) {
-        int[] a = new int[]{1,2,3,4};
-        int r=1;
-        int k = r% a.length;
-        reverseArray(a,0,a.length-1);
-        reverseArray(a,0,k-1);
-        reverseArray(a,k,a.length-1);
-        System.out.println(Arrays.toString(a));
+        int[] array = new int[]{1, 2, 3, 4};
+        int rotations = 1;
+        int k = rotations % array.length;
+
+        // Reverse the entire array
+        reverseArray(array, 0, array.length - 1);
+        // Reverse the first k elements
+        reverseArray(array, 0, k - 1);
+        // Reverse the remaining elements
+        reverseArray(array, k, array.length - 1);
+
+        System.out.println(Arrays.toString(array));
     }
 }
