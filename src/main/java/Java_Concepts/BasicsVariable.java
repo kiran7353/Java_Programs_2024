1757344588bd817cf0347f80e4b6d0c092ae0671
kiran <kbellad3@gmail.com>
2025-02-05 11:11:42 +0530

    This is my latest commit

---
+++ src/main/java/Java_Concepts/BasicsVariable.java
@@ -0,0 +1,7 @@
+package Java_Concepts;
+
+public class BasicsVariable {
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/main/java/Selenium_Scenarios_Interview.java
@@ -0,0 +1,25 @@
+package Course_JAVA;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium_Scenarios_Interview {
+    public static void main(String[] args){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+
+        // Wait for a few seconds to let the page load
+        try {
+            Thread.sleep(5000); // 5 seconds wait (not recommended for production use, use WebDriverWait instead)
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Close the browser
+        driver.quit();
+
+    }
+}
